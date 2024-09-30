c39d2bd0f05d8d1895780b40eb16c2919180df21
Shon Qwezon <shonzbso@gmail.com>
2024-09-30 18:18:26 +0300

    Add comments

--- src/Expression.java
+++ src/Expression.java
@@ -1,39 +1,73 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.Stack;
 
+/**
+ * Class for calculating expression of tokens
+ */
 public class Expression {
-    static public double calc(ArrayList<Token> tokens) throws Exception {
+    private final Scanner sc = new Scanner(System.in);
+    private final String INPUT_DEF = "\nВведите значение для матрицы '%s':\n";
+    private ArrayList<Token> tokens;
+
+    /**
+     * @param tokens Array of tokens
+     */
+    public void loadTokens(ArrayList<Token> tokens) {
+        // Getting matrix values for VAR
+        for (Token token : tokens) {
+            if (token.getState() != Token.State.VAR) continue;
+            // Init token from hash table
+            if (Token.getVarIfDefined(token.getName()) != Token.NULL_VALUE) {
+                token.setValue(Token.getVarIfDefined(token.getName()));
+                continue;
+            }
+            System.out.printf(INPUT_DEF, token.getName());
+            token.setValue(sc.nextInt());
+            Token.defineVar(token.getName(), token.getValue());
+            sc.nextLine();
+        }
+        this.tokens = tokens;
+    }
+
+
+    /** Shunting Yard algorithm
+     * @return Result
+     * @throws Exception Error during calculating
+     */
+    public double calc() throws Exception {
         Stack<Token> opersStack = new Stack<>();
         Stack<Token> argsStack = new Stack<>();
 
         for (Token token : tokens) {
-            Token.State state = token.getState();
-            if (state == Token.State.VAR || state == Token.State.KF)
-                argsStack.push(token);
-            else if (state == Token.State.BRACKET) {
-                if (token.getName().equals(")")) {
-                    if (opersStack.isEmpty())
-                        throw new Exception("Некорректное выражение");
-                    Token oper = opersStack.pop();
-                    while (!oper.getName().equals("(")) {
+            switch (token.getState()) {
+                case Token.State.VAR, Token.State.KF -> argsStack.push(token);
+                case Token.State.BRACKET -> {
+                    if (token.getName().equals(")")) {
+                        if (opersStack.isEmpty())
+                            throw new Exception("Некорректное выражение");
+                        Token oper = opersStack.pop();
+                        while (!oper.getName().equals("(")) {
+                            Token token2 = argsStack.pop();
+                            Token token1 = argsStack.pop();
+                            argsStack.push(applyOperation(oper, token1, token2));
+                            oper = opersStack.pop();
+                        }
+                    } else if (token.getName().equals("("))
+                        opersStack.push(token);
+                    else {
+                        // Handling "|"
+                    }
+                }
+                case Token.State.OPERATOR -> {
+                    if (!opersStack.isEmpty() && opersStack.peek().getState() == Token.State.OPERATOR
+                            && opersStack.peek().getPriority() >= token.getPriority()) {
                         Token token2 = argsStack.pop();
                         Token token1 = argsStack.pop();
-                        argsStack.push(applyOperation(oper, token1, token2));
-                        oper = opersStack.pop();
+                        argsStack.push(applyOperation(opersStack.pop(), token1, token2));
                     }
-                } else if (token.getName().equals("("))
                     opersStack.push(token);
-                else {
-                    // Handling "|"
                 }
-            } else if (state == Token.State.OPERATOR) {
-                if (!opersStack.isEmpty() && opersStack.peek().getState() == Token.State.OPERATOR
-                        && opersStack.peek().getPriority() >= token.getPriority()) {
-                    Token token2 = argsStack.pop();
-                    Token token1 = argsStack.pop();
-                    argsStack.push(applyOperation(opersStack.pop(), token1, token2));
-                }
-                opersStack.push(token);
             }
         }
 
@@ -48,7 +82,14 @@ public class Expression {
         return (token.getState() == Token.State.VAR) ? token.getValue() : Double.parseDouble(token.getName());
     }
 
-    static private Token applyOperation(Token operation, Token token1, Token token2) {
+
+    /**
+     * @param operation
+     * @param token1 First argument
+     * @param token2 Second argument
+     * @return Updated token
+     */
+    private Token applyOperation(Token operation, Token token1, Token token2) {
         double arg1 = (token1.getState() == Token.State.VAR) ? token1.getValue() : Double.parseDouble(token1.getName());
         double arg2 = (token2.getState() == Token.State.VAR) ? token2.getValue() : Double.parseDouble(token2.getName());
         double value = switch (operation.getName()) {
@@ -57,7 +98,7 @@ public class Expression {
             case "*" -> arg1 * arg2;
             case "/" -> arg1 / arg2;
             case "^" -> Math.pow(arg1, arg2);
-            default -> throw new IllegalStateException("Unexpected value: " + operation.getName());
+            default -> throw new IllegalStateException("Неизвестный оператор: " + operation.getName());
         };
 
         return new Token(value);
--- src/Token.java
+++ src/Token.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class for working with tokens
+ */
 public class Token {
     public enum State {
         NONE,
@@ -11,8 +14,15 @@ public class Token {
         BRACKET
     }
 
+    /**
+     * Class for generating tokens
+     */
     static class Tokenizer {
+        /**
+         * Map for storing operator's priority
+         */
         private static final Map<Character, Integer> operators = new HashMap<>();
+        private ArrayList<Token> tokens;
 
         static {
             operators.put('+', 1);
@@ -22,71 +32,91 @@ public class Token {
             operators.put('^', 3);
         }
 
-        private void updateLastTokenName(ArrayList<Token> tokens, char s) {
+        /**
+         * @param s Symbol that is appended to a last token name
+         */
+        private void updateLastTokenName(char s) {
             int lastIndex = tokens.size() - 1;
             Token lastToken = tokens.get(lastIndex);
-            if(lastToken.name.charAt(0) == 'i' && lastToken.state == State.KF)
+            // If the token starts with i, and the next symbol is appending, then it is a VAR now (instead of KF)
+            if (lastToken.name.charAt(0) == 'i' && lastToken.state == State.KF)
                 lastToken.state = State.VAR;
             lastToken.name += s;
             tokens.set(lastIndex, lastToken);
         }
 
-         public ArrayList<Token> run(final String input) throws Exception {
-            ArrayList<Token> tokens = new ArrayList<>();
-
+        /**
+         * @param input String to tokenize
+         * @return ArrayList of tokens
+         * @throws Exception An error in expression
+         */
+        public ArrayList<Token> run(final String input) throws Exception {
+            tokens = new ArrayList<>();
+            // Initial state
             State lastState = State.NONE;
             for (int i = 0; i < input.length(); i++) {
                 char s = input.charAt(i);
                 State currentState = getCharState(s, i + 1);
-                if (currentState != State.NONE)
-                    switch (lastState) {
-                        case NONE -> {
-                            if (s == '-' && tokens.isEmpty()) {
-                                tokens.add(new Token(State.KF, "-1"));
-                                tokens.add(new Token(State.OPERATOR, '*'));
-                            } else
-                                tokens.add(new Token(currentState, s));
-                        }
-                        case VAR -> {
-                            // Update previous VAR
-                            if (currentState == State.VAR || currentState == State.KF) {
-                                updateLastTokenName(tokens, s);
-                                currentState = State.VAR;
-                            }
-                            // Start the new VAR
-                            else
-                                tokens.add(new Token(currentState, s));
-                        }
-                        case KF -> {
-                            if (currentState == State.KF || s == 'i')
-                                updateLastTokenName(tokens, s);
-                            else if (currentState == State.VAR || tokens.getLast().name.charAt(0) == 'i')
-                                updateLastTokenName(tokens, s);
-                            else
-                                tokens.add(new Token(currentState, s));
-                        }
-
-                        case OPERATOR -> {
-                            if (currentState == State.VAR && s == 'T') {
-                                updateLastTokenName(tokens, s);
-                                currentState = State.OPERATOR;
-                            } else
-                                tokens.add(new Token(currentState, s));
-                        }
-                        case BRACKET -> {
-                            if (s == '-') {
-                                tokens.add(new Token(State.KF, "-1"));
-                                tokens.add(new Token(State.OPERATOR, '*'));
-                            } else
-                                tokens.add(new Token(currentState, s));
-                        }
+                switch (lastState) {
+                    case NONE -> {
+                        // Skip NULL symbols, except initial state
+                        if (currentState == State.NONE) break;
+                        // Add extra tokens for handling unary '-'
+                        if (s == '-' && tokens.isEmpty()) {
+                            tokens.add(new Token(State.KF, "-1"));
+                            tokens.add(new Token(State.OPERATOR, '*'));
+                        } else
+                            tokens.add(new Token(currentState, s));
                     }
+                    case VAR -> {
+                        // Update previous VAR
+                        if (currentState == State.VAR || currentState == State.KF) {
+                            updateLastTokenName(s);
+                            currentState = State.VAR;
+                        }
+                        // Start the new VAR
+                        else
+                            tokens.add(new Token(currentState, s));
+                    }
+                    case KF -> {
+                        if (currentState == State.KF || s == 'i')
+                            updateLastTokenName(s);
+                        else if (currentState == State.VAR || tokens.getLast().name.charAt(0) == 'i')
+                            updateLastTokenName(s);
+                        else
+                            tokens.add(new Token(currentState, s));
+                    }
+
+                    case OPERATOR -> {
+                        // Add special operator for matrix
+                        if (currentState == State.VAR && s == 'T') {
+                            updateLastTokenName(s);
+                            currentState = State.OPERATOR;
+                        } else
+                            tokens.add(new Token(currentState, s));
+                    }
+                    case BRACKET -> {
+                        // Add extra tokens for handling unary '-'
+                        if (s == '-') {
+                            tokens.add(new Token(State.KF, "-1"));
+                            tokens.add(new Token(State.OPERATOR, '*'));
+                        } else
+                            tokens.add(new Token(currentState, s));
+                    }
+                }
                 lastState = currentState;
             }
 
             return tokens;
         }
 
+
+        /**
+         * @param s     Char to determine its type (state)
+         * @param index Index in order to show position of an unexpected symbol
+         * @return State of the char
+         * @throws Exception Bumped into an unexpected symbol
+         */
         private State getCharState(char s, int index) throws Exception {
             if (Character.isSpaceChar(s)) return State.NONE;
             else if (Character.isDigit(s) || s == '.' || s == 'i') return State.KF;
@@ -105,7 +135,7 @@ public class Token {
     static public Map<String, Double> varValues = new HashMap<>();
     static public double NULL_VALUE = -1_000_000_000;
 
-    static public double isDefinedVar(String name) {
+    static public double getVarIfDefined(String name) {
         return varValues.getOrDefault(name, NULL_VALUE);
     }
 
@@ -118,7 +148,7 @@ public class Token {
     }
 
     public Token(State state, String name, int priority, double value) {
-        if(state == State.OPERATOR)
+        if (state == State.OPERATOR)
             priority = Tokenizer.operators.get(name.charAt(0));
         this.state = state;
         this.name = name;
